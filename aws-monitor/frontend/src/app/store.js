9102891341fe9712e7c7b3c0f08b4790e0252582
pradeepnp <pradeepnp100@gmail.com>
2024-12-16 20:50:37 +0530

    Initial commit

--- aws-monitor/frontend/src/app/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import awsServicesReducer from '../features/awsServices/awsServicesSlice';
-
-export const store = configureStore({
-  reducer: {
-    awsServices: awsServicesReducer,
-  },
-});
